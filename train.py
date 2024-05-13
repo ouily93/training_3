9837c489fce340cf77d4f98bf4a322a71af8c776
unknown <ouily@example.com>
2024-05-13 18:54:35 +0200

    add lstm model

--- train.py
+++ train.py
@@ -1,1 +1,8 @@
-print("hello")
+import tensorflow as tf 
+from tensorflow.keras.layers import Dense,Birectional,LSTM
+from tensorflow.keras.data.sets import imdb
+
+
+num_words= 10000 # nombre de mot à utiliser
+maxlen =100 # longueur maximale de chaque avis
+(x_train,y_train),(x_test, y_test)=imdb.load_data(num_words=num_words)
